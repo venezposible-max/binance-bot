17e769aae5cb403c1aa707926d98d539bab3b916
infogadgets2024 <infogadgets2024@gmail.com>
2026-02-19 22:11:22 -0400

    Fix useRef missing import

--- src/components/WalletCard.jsx
+++ src/components/WalletCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, forwardRef, useImperativeHandle } from 'react';
+import React, { useState, useEffect, forwardRef, useImperativeHandle, useRef } from 'react';
 import { API_BASE } from '../config/api';
 import styles from './WalletCard.module.css';
 
