1b88890fb25cd0bdb7212f0e1cb862c2e4255213
infogadgets2024 <infogadgets2024@gmail.com>
2026-05-15 19:04:56 -0400

    feat: Convertir app en PWA (Instalable en móvil) con icono holográfico

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'p2p-radar-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/icon.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
